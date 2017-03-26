a77ee8a50addeb1bc9204cf4c181818aa75bb48f
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-03-25 20:23:51 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var server = express();
+var port = process.env.PORT || 8080;
+var mongoURI = process.env.MONGOURI || require('./secrets').MONGOURI;
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+server.use(express.static(__dirname + '/public'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(mongoURI);
+
+server.get('/', function(req, res){
+  res.sendFile('index.html', {root: __dirname + '/public/html'});
+});
+
+server.listen(port, function(){
+  console.log('Now listening on port', port);
+});
